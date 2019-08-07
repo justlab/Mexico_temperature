6536fc17641361f6c6a1ebd00a360475df07a4d6
Kodi Arfer <git@arfer.net>
2019-08-07 12:23:33 -0400

    Spun off `deleg.weighted.preds` into a new file.

---
+++ code/delegaciones.R
@@ -0,0 +1,59 @@
+suppressPackageStartupMessages(
+   {library(data.table)
+    library(sf)})
+
+source("common.R")
+
+mexico.city.agebs.path = "~/Jdrive/PM/Just_Lab/projects/airmex/data/gis/gisdata/AGEBS_CDMX_2010.shp"
+population.path.fmt = "~/Jdrive/PM/Just_Lab/projects/airmex/data/population/%s_cuadratic_csv"
+
+deleg.weighted.preds = function()
+  # Predict temperature per day and delegación (subregion of
+  # Mexico City), weighted by the population of each delegación.
+  # Predictions are made with weights from people of all ages and
+  # from people aged 65 and above.
+   {message("Loading population counts")
+    d = rbindlist(unlist(rec = F, lapply(c(F, T), function(only.65plus) lapply(
+        list.files(full.names = T, sprintf(population.path.fmt,
+            (if (only.65plus) "65" else "Total"))),
+        function(s)
+           {d = subset(fread(s), select = -TOTAL)
+            d[, date := as.Date(date, format = "%m/%d/%Y")]
+            d = d[year(date) %in% available.years]
+            setnames(d, names(d), str_replace(names(d), fixed("_"), ""))
+            cbind(melt(d, id = "date", var = "ageb", val = "pop"),
+                only.65plus = only.65plus)}))))
+
+    # The population numbers we have are per AGEB (Área Geoestadística
+    # Básica, or Basic Geostatistical Area), which are subregions of
+    # delegaciones.
+    message("Loading AGEB definitions")
+    d = local(
+       {agebs = read_sf(mexico.city.agebs.path)
+        agebs = data.table(
+            ageb = factor(agebs$CVEGEO),
+            # For each AGEB, choose a cell of the master grid by finding the
+            # cell closest to the AGEB's centroid.
+            mrow = get.knnx(k = 1,
+                master.grid[, .(x_sinu, y_sinu)],
+                st_coordinates(st_transform(crs = crs.satellite,
+                    st_centroid(agebs$geometry))))$nn.index[,1])
+        stopifnot(identical(levels(agebs$ageb), levels(d$ageb)))
+        merge(d, agebs, by = "ageb", all = T)})
+    stopifnot(!anyNA(d))
+
+    d = merge(d, by = c("date", "mrow"), all = T, rbindlist(lapply(
+        unique(year(d$date)),
+        function(y)
+           {message("Loading predictions: ", y)
+            p = predict.temps(y, "pred.area")[mrow %in% d$mrow]
+            p[, date := as.Date(paste0(y, "-01-01")) + (yday - 1)]
+            p[, yday := NULL]
+            p[date %in% d$date]})))
+    stopifnot(!anyNA(d))
+
+    message("Summarizing by delegación")
+    d[, by = .(only.65plus, date, delegacion = factor(substr(ageb, 1, 5))),
+        .SDcols = paste0("pred.", temp.ground.vars),
+        lapply(.SD, function(temp) sum(temp * pop)/sum(pop))]}
+deleg.weighted.preds = pairmemo(deleg.weighted.preds, pairmemo.dir, fst = T)
--- code/modeling.R
+++ code/modeling.R
@@ -46,8 +46,6 @@ c(full.satellite.grid, get.satellite.data, get.elevation) %<-% local(
     list(full.satellite.grid, get.satellite.data, get.elevation)})
 
 all.agebs.year = 2010L
-mexico.city.agebs.path = "~/Jdrive/PM/Just_Lab/projects/airmex/data/gis/gisdata/AGEBS_CDMX_2010.shp"
-population.path.fmt = "~/Jdrive/PM/Just_Lab/projects/airmex/data/population/%s_cuadratic_csv"
 
 plan(multiprocess)
 
@@ -587,57 +585,6 @@ predict.temps.at = function(fname, date.col, lon.col, lat.col)
         paste0("model.", temp.ground.vars),
         paste0("nearest.", temp.ground.vars),
         "precipitation.mm"))]}
-
-deleg.weighted.preds = function()
-  # Predict temperature per day and delegación (subregion of
-  # Mexico City), weighted by the population of each delegación.
-  # Predictions are made with weights from people of all ages and
-  # from people aged 65 and above.
-   {message("Loading population counts")
-    d = rbindlist(unlist(rec = F, lapply(c(F, T), function(only.65plus) lapply(
-        list.files(full.names = T, sprintf(population.path.fmt,
-            (if (only.65plus) "65" else "Total"))),
-        function(s)
-           {d = subset(fread(s), select = -TOTAL)
-            d[, date := as.Date(date, format = "%m/%d/%Y")]
-            d = d[year(date) %in% available.years]
-            setnames(d, names(d), str_replace(names(d), fixed("_"), ""))
-            cbind(melt(d, id = "date", var = "ageb", val = "pop"),
-                only.65plus = only.65plus)}))))
-
-    # The population numbers we have are per AGEB (Área Geoestadística
-    # Básica, or Basic Geostatistical Area), which are subregions of
-    # delegaciones.
-    message("Loading AGEB definitions")
-    d = local(
-       {agebs = read_sf(mexico.city.agebs.path)
-        agebs = data.table(
-            ageb = factor(agebs$CVEGEO),
-            # For each AGEB, choose a cell of the master grid by finding the
-            # cell closest to the AGEB's centroid.
-            mrow = get.knnx(k = 1,
-                master.grid[, .(x_sinu, y_sinu)],
-                st_coordinates(st_transform(crs = crs.satellite,
-                    st_centroid(agebs$geometry))))$nn.index[,1])
-        stopifnot(identical(levels(agebs$ageb), levels(d$ageb)))
-        merge(d, agebs, by = "ageb", all = T)})
-    stopifnot(!anyNA(d))
-
-    d = merge(d, by = c("date", "mrow"), all = T, rbindlist(lapply(
-        unique(year(d$date)),
-        function(y)
-           {message("Loading predictions: ", y)
-            p = predict.temps(y, "pred.area")[mrow %in% d$mrow]
-            p[, date := as.Date(paste0(y, "-01-01")) + (yday - 1)]
-            p[, yday := NULL]
-            p[date %in% d$date]})))
-    stopifnot(!anyNA(d))
-
-    message("Summarizing by delegación")
-    d[, by = .(only.65plus, date, delegacion = factor(substr(ageb, 1, 5))),
-        .SDcols = paste0("pred.", temp.ground.vars),
-        lapply(.SD, function(temp) sum(temp * pop)/sum(pop))]}
-deleg.weighted.preds = pairmemo(deleg.weighted.preds, pairmemo.dir, fst = T)
 
 per.mrow.population = function(pop.col)
    {message("Making subgrid")
